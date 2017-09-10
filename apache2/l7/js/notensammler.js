11e136afeb97437cceb549885ac4d63448e8332d
buzzruckz <buzz.ruckz@web.de>
2017-09-10 16:25:00 +0200

    Layout1
    Layout1 added - Please vote

---
+++ apache2/l7/js/notensammler.js
@@ -0,0 +1,9 @@
+<script>
+// Load this when the DOM is ready
+$(function(){
+  // You used .myCarousel here.
+  // That's the class selector not the id selector,
+  // which is #myCarousel
+  $('#notensammlerCarousel').carousel();
+});
+</script>
